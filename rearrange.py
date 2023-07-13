d269c240a5610f58f0eece80060d5f15e5c5ef2d
Arnob <74468064+arnob016@users.noreply.github.com>
2023-07-13 23:41:00 +0600

    Update rearrange.py
    Hello, here's how you rearrange an array in python if this is what you were looking for.

--- rearrange.py
+++ rearrange.py
@@ -1,1 +1,14 @@
+#Hello, here's how you rearrange an array in python if this is what you were looking for.
 
+def rearrange_array(numbers):
+    positives = [num for num in numbers if num >= 0]
+    negatives = [num for num in numbers if num < 0]
+
+    rearranged_numbers = negatives + positives
+
+    return rearranged_numbers
+
+# Here's an array of numbers
+numbers = [3, -2, 1, -5, 4, -6, 8, 0, -3]
+rearranged = rearrange_array(numbers)
+print(rearranged)
